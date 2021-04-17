c659efe466cd38cb0a0771dcda9186e8f13dde9a
kyson zhu <zjh171@qq.com>
2021-04-17 16:03:42 +0800

    [ADD] nsobject + runtime

--- Classes/GJTAdditionKit.h
+++ Classes/GJTAdditionKit.h
@@ -13,6 +13,7 @@
 #import <GJTAdditionKit/UIColor+GJTAddition.h>
 #import <GJTAdditionKit/UIImage+GJTAddition.h>
 #import <GJTAdditionKit/UIDevice+HandyValue.h>
+#import <GJTAdditionKit/NSObject+GJTAssociatedObject.h>
 
 #import <GJTAdditionKit/UINavigationController+GJTFullscreenPopGesture.h>
 
